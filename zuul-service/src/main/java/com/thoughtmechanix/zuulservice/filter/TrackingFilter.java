445517b424a12d0dae2c3b53fe328dd4c67d526d
Han-Ming.Li <noah.atlantis.lee@hotmail.com>
2019-11-24 20:04:49 +0800

    commit

--- zuul-service/src/main/java/com/thoughtmechanix/zuulservice/filter/TrackingFilter.java
+++ zuul-service/src/main/java/com/thoughtmechanix/zuulservice/filter/TrackingFilter.java
@@ -4,7 +4,10 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.exception.ZuulException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class TrackingFilter extends ZuulFilter {
@@ -13,23 +16,39 @@ public class TrackingFilter extends ZuulFilter {
 	private static final boolean SHOULD_FILTER = true;
 	private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
 
+	@Autowired
+	FilterUtils filterUtils;
+
 	@Override
 	public String filterType() {
-		return null;
+		return FilterUtils.PRE_FILTER_TYPE;
 	}
 
 	@Override
 	public int filterOrder() {
-		return 0;
+		return FILTER_ORDER;
 	}
 
 	@Override
 	public boolean shouldFilter() {
-		return false;
+		return SHOULD_FILTER;
 	}
 
 	@Override
 	public Object run() throws ZuulException {
+
 		return null;
 	}
+
+	private boolean isCorrelationIdPresent() {
+		if (filterUtils.getCorrelationId() != null) {
+			return true;
+		}
+		return false;
+	}
+
+	private String generateCorrelationId() {
+		return UUID.randomUUID().toString();
+	}
+
 }
